ebb8383c0a1283540ea7489453dde3366d71e47a
Ardennata <88446225+Ardennata@users.noreply.github.com>
2024-05-03 20:31:56 +0700

    first commit

---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  late String questionText;
+  late bool questionAnswer;
+
+  Question(this.questionText, this.questionAnswer);
+
+  // Question(String questionText, bool questionAnswer) {
+  //   this.questionText = questionText;
+  //   this.questionAnswer = questionAnswer;
+  // }
+}
